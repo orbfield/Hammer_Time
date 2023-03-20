24a41c5a4c540cd44d82fd4fffa09e2aaf5633dc
orbfield <127656509+orbfield@users.noreply.github.com>
2023-03-20 22:30:38 +0000

    Add files via upload
    working fm synth

--- HammerSound.cpp
+++ HammerSound.cpp
@@ -3,13 +3,11 @@
 #include <cmath>
 #include <vector>
 #include <thread>
-#include <mutex>
-#include <opencv2/opencv.hpp>
 #include "HammerSound.h"
 #include <RtAudio.h>
 #include <C:/Dev/Iibaries/rtmidi-5.0.0/RtMidi.h>
-#include <fstream>
 #include <atomic>
+#include <fstream>
 
 HammerSound::HammerSound() : isPlaying(false), currentFrequency(0.0), amplitude(0.0) {}
 HammerSound::~HammerSound() {}
@@ -18,6 +16,34 @@ int audioCallback(void* outputBuffer, void* inputBuffer, unsigned int nBufferFra
     double streamTime, RtAudioStreamStatus status, void* userData) {
     HammerSound* hammerSound = static_cast<HammerSound*>(userData);
     return hammerSound->processAudioOutput(outputBuffer, inputBuffer, nBufferFrames, streamTime, status);
+}
+
+
+void midiInputCallback(double deltatime, std::vector<unsigned char>* message, void* userData) {
+    // Cast userData to HammerSound object
+    HammerSound* hammerSound = static_cast<HammerSound*>(userData);
+
+    unsigned int nBytes = message->size();
+
+    if (nBytes > 0) {
+        unsigned char statusByte = message->at(0);
+
+        if (statusByte == 0x90 && nBytes == 3) { // Note On message
+            int midiPitch = message->at(1);
+            int midiVelocity = message->at(2);
+
+            if (midiVelocity > 0) {
+                hammerSound->sendNoteOn(midiPitch, midiVelocity);
+            }
+            else {
+                hammerSound->sendNoteOff(midiPitch);
+            }
+        }
+        else if (statusByte == 0x80 && nBytes == 3) { // Note Off message
+            int midiPitch = message->at(1);
+            hammerSound->sendNoteOff(midiPitch);
+        }
+    }
 }
 
 void handleError(const char* errorText, void* userData) {
@@ -35,9 +61,8 @@ void HammerSound::sendTestBeep() {
 }
 
 void HammerSound::run() {
-
-
-    // Initialize audio output
+ 
+    // Initialize audio output and midi input
     try {
         auto audioOut = std::make_unique<RtAudio>();
 
@@ -61,35 +86,57 @@ void HammerSound::run() {
         audioOut->startStream();
 
         sendTestBeep();
+
+        try {
+            auto midiIn = std::make_unique<RtMidiIn>();
+
+            if (midiIn->getPortCount() == 0) {
+                std::cout << "No MIDI input ports available!" << std::endl;
+                return;
+            }
+
+            midiIn->openPort(0);
+            midiIn->setCallback(&midiInputCallback, this);
+            midiIn->ignoreTypes(false, false, false);
+            std::cout << "MIDI input opened on port: " << midiIn->getPortName(0) << std::endl;
+          
+            char input;
+            std::cout << "Press any key and hit enter to quit." << std::endl;
+            std::cin >> input;
+            midiIn->closePort();
+            audioOut->closeStream();
+        }
+        catch (RtMidiError& error) {
+            error.printMessage();
+            return;
+        }
+        catch (...) {
+            std::cerr << "Unknown error occurred while initializing RtMidi.\n";
+            return;
+        }
+        
     }
     catch (...) {
-        std::cerr << "Unknown error occurred while initializing RtAudio." << std::endl;
+        std::cerr << "Unknown error occurred while initializing RtAudio.\n";
         return;
     }
 }
+
 void HammerSound::sendNoteOn(int midi_pitch, int midi_velocity) {
     isPlaying.store(true);
-
-    // Update current frequency
-    currentFrequency.store(146.83 * pow(2.0, (midi_pitch - 50) / 12.0));
-
-    // Set amplitude based on MIDI velocity
+       
+    currentFrequency.store(146.83 * pow(2.0, (midi_pitch - 50) / 12.0)); 
     amplitude.store(midi_velocity / 127.0);
-
-    // Debug print statement
+    
     std::cout << "Note ON: Midi pitch = " << midi_pitch << ", Midi velocity = " << midi_velocity << ", Frequency = " << currentFrequency.load() << " Hz\n";
 }
 
 void HammerSound::sendNoteOff(int midi_pitch) {
     isPlaying.store(false);
-
-    // Reset current frequency
+    
     currentFrequency.store(0.0);
-
-    // Debug print statement
-    std::cout << "Note OFF: Midi pitch = " << midi_pitch << std::endl;
+    std::cout << "Note OFF: Midi pitch = " << midi_pitch << " \n";
 }
-
 
 // Process audio output
 int HammerSound::processAudioOutput(void* outputBuffer, void* inputBuffer, unsigned int nBufferFrames,
@@ -100,10 +147,21 @@ int HammerSound::processAudioOutput(void* outputBuffer, void* inputBuffer, unsig
     double sampleRate = 44100.0;
     double phaseIncrement = 2.0 * M_PI * currentFrequency.load() / sampleRate;
     static double phase = 0.0;
+    double carrierFrequency = currentFrequency.load();
+    double modulatorFrequency = carrierFrequency * 1.5; // You can experiment with this value
+    double modulatorAmplitude = 0.1; // You can experiment with this value
+
 
     if (isPlaying.load()) {
         for (unsigned int i = 0; i < nBufferFrames; ++i) {
-            double sampleValue = amplitude.load() * (sin(phase) >= 0 ? 1.0 : -1.0);
+            static double modulatorPhase = 0.0;
+            double modulatorSignal = modulatorAmplitude * sin(modulatorPhase);
+            double sampleValue = amplitude.load() * sin(phase + modulatorSignal);
+
+            modulatorPhase += 2.0 * M_PI * modulatorFrequency / sampleRate;
+            if (modulatorPhase > 2.0 * M_PI) {
+                modulatorPhase -= 2.0 * M_PI;
+            }
             outBuffer[i * 2] = sampleValue; // Left channel
             outBuffer[i * 2 + 1] = sampleValue; // Right channel
             phase += phaseIncrement;
@@ -111,10 +169,6 @@ int HammerSound::processAudioOutput(void* outputBuffer, void* inputBuffer, unsig
                 phase -= 2.0 * M_PI;
             }
         }
-        // Debug print statements - write to file
-        std::ofstream debugFile("debug.txt", std::ios_base::app);
-        debugFile << "sampleValue: " << outBuffer[0] << ", phase: " << phase << ", phaseIncrement: " << phaseIncrement << ", currentFrequency: " << currentFrequency << std::endl;
-        debugFile.close();
     }
     else {
         memset(outBuffer, 0, nBufferFrames * 2 * sizeof(double));
